d15e416e64b8bd829bebfefce997c7f20c8c2633
rahul <kamo.rahul@gmail.com>
2016-11-15 16:10:02 +0530

    ok

--- server.js
+++ server.js
@@ -15,6 +15,11 @@ MongoClient.connect('mongodb://127.0.0.1:27017/category', (err, database) => {
   })
 })
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
